03afb18c7db4bf3ad3007379b6d8756aefd64b9b
Aashutosh Kumar Singh <aashutoshkumarsingh71@gmail.com>
2025-10-07 17:42:09 +0530

    Update Vehicle.cpp
    Use namespace to make it more readable

--- solutions/cpp/parkinglot/Vehicle.cpp
+++ solutions/cpp/parkinglot/Vehicle.cpp
@@ -1,20 +1,20 @@
 #include "Vehicle.hpp"
 #include <iostream>
-
-Vehicle::Vehicle(std::string licensePlate, VehicleType type, std::string color)
+using namespace std;
+Vehicle::Vehicle(string licensePlate, VehicleType type, string color)
     : licensePlate(licensePlate), type(type), color(color) {}
 
-std::string Vehicle::getLicensePlate() const { return licensePlate; }
+string Vehicle::getLicensePlate() const { return licensePlate; }
 VehicleType Vehicle::getType() const { return type; }
-std::string Vehicle::getColor() const { return color; }
+string Vehicle::getColor() const { return color; }
 
 void Vehicle::displayInfo() const {
-    std::cout << "Vehicle: " << color << " ";
+    cout << "Vehicle: " << color << " ";
     switch (type) {
-        case VehicleType::CAR: std::cout << "Car"; break;
-        case VehicleType::MOTORCYCLE: std::cout << "Motorcycle"; break;
-        case VehicleType::TRUCK: std::cout << "Truck"; break;
-        case VehicleType::BUS: std::cout << "Bus"; break;
+        case VehicleType::CAR: cout << "Car"; break;
+        case VehicleType::MOTORCYCLE: cout << "Motorcycle"; break;
+        case VehicleType::TRUCK: cout << "Truck"; break;
+        case VehicleType::BUS: cout << "Bus"; break;
     }
-    std::cout << " (License: " << licensePlate << ")" << std::endl;
+    cout << " (License: " << licensePlate << ")" <<e ndl;
 } 
